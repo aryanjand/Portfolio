6cdf9cd1340ed51d04dbacc60e179650e9510814
aryan <aryan.jand@gmail.com>
2024-05-08 15:08:54 -0700

    Added Spacing bettween Song Cards.

--- app/about-section/SongCarousel.tsx
+++ app/about-section/SongCarousel.tsx
@@ -6,7 +6,7 @@ import "../animations/animate.css";
 const SongCarousel = () => {
   return (
     <div className="animate absolute bottom-5 flex w-[99%] sm:w-[99%] md:w-[99%] lg:w-[99%] xl:w-[99%]">
-      <div className="mx-auto flex w-[50%] justify-around gap-1 lg:my-[1px]">
+      <div className="mx-auto flex w-[50%] justify-around gap-10 lg:my-[1px]">
         {info.favoriteSongs.map((song: SongProps) => (
           <SongCard
             key={song.title}
