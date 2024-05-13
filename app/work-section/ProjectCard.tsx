486b2f4c62a3ef598d16305f00371038c0e33d2b
aryan <aryan.jand@gmail.com>
2024-05-13 15:01:06 -0700

    Project cards improved

--- app/work-section/ProjectCard.tsx
+++ app/work-section/ProjectCard.tsx
@@ -20,133 +20,85 @@ const ProjectCard = ({
 }: ProjectProps) => {
   return (
     <motion.div
-      style={
-        {
-          backgroundColor: "#212531",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center center",
-          position: "relative",
-        } as React.CSSProperties
-      }
-      className={`relative z-10 h-[550px]  w-full items-stretch justify-center overflow-hidden rounded-3xl bg-center py-0 sm:h-[700px] sm:w-[100%] md:h-[650px] md:w-[100%] lg:h-[500px]`}
+      className={`w-full rounded-3xl bg-[#212531] bg-center`}
       initial="initial"
       animate="animate"
     >
-      <Image
-        src={image}
-        alt={name}
-        width={2000}
-        height={2500}
-        className={`absolute -bottom-2 w-[70%] rounded-3xl sm:w-[85%] md:w-[60%] lg:max-w-[55%] ${
-          id % 2 === 0 ? "right-0" : "left-0"
-        }`}
-      />
-      <div
-        className={`absolute top-0 text-[#0E1016] ${
-          id % 2 === 0 ? "left-0 ml-8 lg:ml-14" : "right-0 mr-8 lg:mr-14"
-        } mt-6 flex  items-center justify-center gap-4 lg:mt-10`}
-      >
-        {available ? (
-          <>
-            {available.github ? (
-              <Link
-                href={github}
-                target="_blank"
-                className="rounded-full"
-                aria-label="Open GitHub Repository"
-              >
-                <FontAwesomeIcon
-                  icon={faGithub}
-                  className=" w-[20px] rounded-full bg-white p-5 text-[20px] md:w-[25px] md:text-[24px] lg:w-[30px] lg:text-[28px]"
-                  data-blobity
-                  data-blobity-radius="38"
-                  data-blobity-offset-x="4"
-                  data-blobity-offset-y="4"
-                  data-blobity-magnetic="true"
-                />
-              </Link>
+      <div className="grid grid-cols-5 grid-rows-2">
+        <div
+          className={`p-5 lg:p-7 row-start-1 row-span-1  ${id % 2 === 0 ? "col-span-5 md:col-start-1 md:col-span-2" : "col-span-5 md:col-start-4 md:col-span-2"}`}
+        >
+          <div className={`mt-6 text-[#0E1016] lg:mt-10`}>
+            {available ? (
+              <div className={`flex justify-start gap-4 mb-5`}>
+                <Link
+                  href={github}
+                  target="_blank"
+                  className="rounded-full"
+                  aria-label="Open GitHub Repository"
+                >
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    className="w-[20px] rounded-full bg-white p-5 text-[20px] md:w-[25px] md:text-[24px] lg:w-[30px] lg:text-[28px]"
+                    data-blobity
+                    data-blobity-radius="38"
+                    data-blobity-offset-x="4"
+                    data-blobity-offset-y="4"
+                    data-blobity-magnetic="true"
+                  />
+                </Link>
+                <Link href={demo} target="_blank" aria-label="Open Live Demo">
+                  <FontAwesomeIcon
+                    icon={faLink}
+                    className="w-[20px] rounded-full bg-white p-5 text-[20px] md:w-[25px] md:text-[24px] lg:w-[30px] lg:text-[28px]"
+                    data-blobity
+                    data-blobity-radius="38"
+                    data-blobity-offset-x="4"
+                    data-blobity-offset-y="4"
+                    data-blobity-magnetic="trues"
+                  />
+                </Link>
+              </div>
             ) : (
               <></>
             )}
-            {available.demo ? (
-              <Link href={demo} target="_blank" aria-label="Open Live Demo">
-                <FontAwesomeIcon
-                  icon={faLink}
-                  className=" w-[20px] rounded-full bg-white p-5 text-[20px] md:w-[25px] md:text-[24px] lg:w-[30px] lg:text-[28px]"
-                  data-blobity
-                  data-blobity-radius="38"
-                  data-blobity-offset-x="4"
-                  data-blobity-offset-y="4"
-                  data-blobity-magnetic="trues"
-                />
-              </Link>
-            ) : (
-              <></>
-            )}
-          </>
-        ) : (
-          <div className=" flex items-center justify-center gap-4">
-            <Link
-              href={github}
-              target="_blank"
-              className="mt-1 rounded-full"
-              aria-label="Open GitHub Repository"
-            >
-              <FontAwesomeIcon
-                icon={faGithub}
-                className=" w-[20px]  rounded-full bg-white p-5 text-[20px] md:w-[25px] md:text-[24px] lg:w-[30px] lg:text-[28px]"
-                data-blobity
-                data-blobity-radius="38"
-                data-blobity-offset-x="4"
-                data-blobity-offset-y="4"
-                data-blobity-magnetic="true"
-              />
-            </Link>
-            <div className=" rounded-xl bg-white px-4 py-2 md:px-5 md:py-3 lg:px-6 lg:py-4">
-              <h3 className="text-[16px] md:text-[18px] lg:text-[20px] ">
-                Coming soon
-              </h3>
-            </div>
           </div>
-        )}
-      </div>
-      <div
-        className={`absolute text-white  ${
-          !(id % 2 === 0)
-            ? "right-0 top-32 mr-0 ml-10 md:right-0 md:ml-0 lg:right-0 lg:top-60  lg:mr-4"
-            : "left-10 top-32 ml-0 md:mr-12 lg:top-52 lg:ml-4"
-        } mb-10  md:mb-16 lg:mb-14 `}
-      >
-        <AnimatedTitle
-          text={name}
-          className={
-            "max-w-[90%] text-[40px] leading-none text-white md:text-[44px] md:leading-none lg:max-w-[450px] lg:text-[48px] lg:leading-none"
-          }
-          wordSpace={"mr-[0.25em]"}
-          charSpace={"-mr-[0.01em]"}
-        />
-        <AnimatedBody
-          text={description}
-          className={
-            "mt-4 w-[90%] max-w-[457px] text-[16px] font-semibold text-[#95979D] "
-          }
-        />
-        <div className="mt-9 flex gap-4">
-          {technologies.map((tech, id) => (
-            <AnimatedTitle
-              text={tech}
-              wordSpace={"mr-[0.25em]"}
-              charSpace={"mr-[0.01em]"}
-              key={id}
-              className={
-                "text-[14px] font-bold uppercase md:text-[16px] lg:text-[18px] "
-              }
-            />
-          ))}
+
+          <AnimatedTitle
+            text={name}
+            className={
+              "text-[34px] text-white md:text-[38px] lg:text-[40px]"
+            }
+          />
+          <AnimatedBody
+            text={description}
+            className={
+              "mt-4 text-[12px] md:text-[14px] lg:text-[16px] font-semibold text-[#95979D]"
+            }
+          />
+          <div
+            className={`mt-9 flex gap-4 flex-wrap`}
+          >
+            {technologies.map((tech, id) => (
+              <p
+                key={id}
+                className={"text-[12px] font-bold uppercase md:text-[16px] lg:text-[18px]"}
+              >{tech}</p>
+            ))}
+          </div>
+
+        </div>
+        <div className={`flex justify-center row-start-2 row-span-1 ${id % 2 === 0 ? "col-span-5 md:col-start-2 md:col-span-4" : "col-span-5 md:col-start-1 md:col-span-4"}`}>
+          <Image
+            src={image}
+            alt={name}
+            width={720}
+            height={325}
+            className={`w-full max-h-full h-auto lg:h-96 rounded-3xl`}
+          />
         </div>
       </div>
-    </motion.div>
+    </motion.div >
   );
 };
 
