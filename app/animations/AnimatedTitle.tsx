eb1e09f88f6a2ee7745917b3d75b099e50e2614e
aryan <aryan.jand@gmail.com>
2024-05-13 12:13:08 -0700

    Updated AnimatedTitle removed nowrap

--- app/animations/AnimatedTitle.tsx
+++ app/animations/AnimatedTitle.tsx
@@ -5,18 +5,13 @@ import { useInView } from "react-intersection-observer";
 type AnimatedTitleProps = {
   text: string;
   className: string;
-  wordSpace: string;
-  charSpace: string;
   delay?: number;
 };
 
 export default function AnimatedTitle({
   text,
   className,
-  wordSpace,
-  charSpace,
 }: AnimatedTitleProps) {
-  //   const text = "Animated Text"; // This would normally be passed into this component as a prop!
 
   const ctrls = useAnimation();
 
@@ -34,26 +29,6 @@ export default function AnimatedTitle({
     }
   }, [ctrls, inView]);
 
-  const wordAnimation = {
-    hidden: {},
-    visible: {},
-  };
-
-  const characterAnimation = {
-    hidden: {
-      opacity: 0,
-      y: `0.25em`,
-    },
-    visible: {
-      opacity: 1,
-      y: `0em`,
-      transition: {
-        duration: 1,
-        ease: [0.2, 0.65, 0.3, 0.9],
-      },
-    },
-  };
-
   return (
     <h2 aria-label={text} role="heading" className={className}>
       {text.split(" ").map((word, index) => {
@@ -64,15 +39,12 @@ export default function AnimatedTitle({
             key={index}
             initial="hidden"
             animate={ctrls}
-            variants={wordAnimation}
             transition={{
               delayChildren: index * 0.25,
               staggerChildren: 0.05,
             }}
-            className={`inline-block whitespace-nowrap ${wordSpace}`}
           >
-            {" "}
-            {word}
+            {" "}{word} {index !== text.length - 1 ? " " : null}
           </motion.span>
         );
       })}
