a0b26002ee5389bae2b83e3837366f34cdca9004
aryan <aryan.jand@gmail.com>
2024-05-15 16:56:21 -0700

    PreLoader Mobile Friendly

--- app/animations/PreLoader/PreLoader.tsx
+++ app/animations/PreLoader/PreLoader.tsx
@@ -10,8 +10,8 @@ const PreLoader = () => {
   }, []);
 
   return (
-    <div className="preloader gap-[5px] overflow-hidden text-[14px] sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]">
-      <div className="texts-container w-500 flex h-60 items-center justify-center gap-[5px] overflow-hidden text-[14px] font-extrabold text-[#e4ded7] opacity-0 sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]">
+    <div className="preloader gap-[5px] text-[14px] sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]">
+      <div className="texts-container w-500 flex h-60 flex-col items-center justify-center gap-[5px] overflow-hidden text-[14px] font-extrabold text-[#e4ded7] opacity-0 sm:flex-row sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]">
         {info.personalInfo.roles.map((role) => (
           <span key={role}>{role}</span>
         ))}
