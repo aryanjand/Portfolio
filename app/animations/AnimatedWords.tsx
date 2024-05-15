0b2556f78e70191513810a5fdecad5bf107e6f0a
aryan <aryan.jand@gmail.com>
2024-05-15 16:56:42 -0700

    Fixed Text Formating

--- app/animations/AnimatedWords.tsx
+++ app/animations/AnimatedWords.tsx
@@ -6,9 +6,14 @@ import { useInView } from "react-intersection-observer";
 type AnimatedWordsProps = {
   title: string;
   style: string;
+  hidden?: boolean;
 };
 
-const AnimatedWords: React.FC<AnimatedWordsProps> = ({ title, style }) => {
+const AnimatedWords: React.FC<AnimatedWordsProps> = ({
+  title,
+  style,
+  hidden = false,
+}) => {
   const ctrls = useAnimation();
 
   const { ref, inView } = useInView({
@@ -52,28 +57,28 @@ const AnimatedWords: React.FC<AnimatedWordsProps> = ({ title, style }) => {
   };
 
   return (
-    <h1 aria-label={title} role="heading">
+    <h1
+      aria-label={title}
+      role="heading"
+      className={`${hidden ? "hidden md:block" : ""}`}
+    >
       <motion.span
         ref={ref}
-        className="flex max-w-[500px] flex-col overflow-hidden  text-center text-[96px] font-extrabold  leading-[0.8em] text-[#e4ded7] sm:text-[120px] sm:leading-[0.85em] md:max-w-[900px] md:text-[155.5px] lg:text-[215px]"
+        className="flex text-center font-extrabold text-[#e4ded7]"
       >
-        {title.split(" ").map((word, index) => (
-          <motion.div
-            key={index}
-            initial="initial"
-            animate={ctrls}
-            // variants={staggerChildren}
-            transition={{
-              delayChildren: index * 0.25,
-              staggerChildren: 0.05,
-            }}
-            className="flex items-center justify-center overflow-hidden"
-          >
-            <motion.span className={style} variants={wordAnimation}>
-              {word + "\u00A0"}
-            </motion.span>
-          </motion.div>
-        ))}
+        <motion.div
+          initial="initial"
+          animate={ctrls}
+          transition={{
+            delayChildren: 0.25,
+            staggerChildren: 0.05,
+          }}
+          className="flex items-center justify-center overflow-hidden"
+        >
+          <motion.span variants={wordAnimation} className={style}>
+            {title}
+          </motion.span>
+        </motion.div>
       </motion.span>
     </h1>
   );
--- app/animations/AnimatedWords2.tsx
+++ app/animations/AnimatedWords2.tsx
@@ -42,29 +42,26 @@ const AnimatedWords2: React.FC<AnimatedWords2Props> = ({ title, style }) => {
   };
 
   return (
-    <h1 aria-label={title} role="heading">
+    <h3 aria-label={title} role="heading">
       <motion.span className={style} ref={ref}>
-        {title.split(" ").map((word, index) => (
-          <motion.div
-            key={index}
-            initial="initial"
-            animate={ctrls}
-            className="flex items-center justify-center overflow-hidden last:-mr-10"
-            transition={{
-              delayChildren: index * 0.25,
-              staggerChildren: 0.05,
-            }}
+        <motion.div
+          initial="initial"
+          animate={ctrls}
+          className="flex items-center justify-center last:-mr-10"
+          transition={{
+            delayChildren: 0.25,
+            staggerChildren: 0.05,
+          }}
+        >
+          <motion.span
+            className="-mb-4 inline-block pt-1 sm:-mb-2 md:-mb-3 lg:-mb-4"
+            variants={wordAnimation2}
           >
-            <motion.span
-              className="-mb-4 inline-block overflow-hidden pt-1 sm:-mb-2 md:-mb-3 lg:-mb-4"
-              variants={wordAnimation2}
-            >
-              {word + "\u00A0"}
-            </motion.span>
-          </motion.div>
-        ))}
+            {title}
+          </motion.span>
+        </motion.div>
       </motion.span>
-    </h1>
+    </h3>
   );
 };
 
