ad379d5d811032d527ea1a43c6eb7919c050f3e5
aryan <aryan.jand@gmail.com>
2024-05-15 16:58:30 -0700

    Removed some nav items

--- app/navbar/NavBar.tsx
+++ app/navbar/NavBar.tsx
@@ -1,13 +1,11 @@
 "use client";
 import Link from "next/link";
-import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import info from "../../profile.json";
 
 const NavBar = () => {
   const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     // first prevent the default behavior
-    e.preventDefault();
+    // e.preventDefault();
     // get the href and remove everything before the hash (#)
     const href = e.currentTarget.href;
     const targetId = href.replace(/.*\#/, "");
@@ -19,31 +17,17 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="fixed bottom-10 left-0 right-0 z-50 my-0  mx-auto  flex w-[306px] items-center justify-center gap-1 rounded-lg bg-[#07070a]/90 px-1 py-1 text-[#e4ded7] backdrop-blur-md sm:w-[383.3px] md:p-2 lg:w-[391.3px]">
+    <nav className="fixed bottom-10 left-0 right-0 z-50 my-0 mx-auto flex w-[306px] items-center justify-center gap-1 rounded-lg bg-[#07070a]/90 px-1 py-1 text-[#e4ded7] backdrop-blur-md sm:w-[383.3px] md:p-2 lg:w-[391.3px]">
       <Link
         href={info.personalInfo.resumeLink}
         target="_blank"
-        className="flex"
-        aria-label="Open my resume"
-        data-blobity-tooltip="View Resume"
-        data-blobity-magnetic="false"
+        aria-label="View Resume"
       >
-        <FontAwesomeIcon
-          icon={faFilePdf}
-          className="py-2 px-2 text-[16px] sm:px-4 md:py-1"
-        />
-      </Link>
-
-      <Link
-        href="#home"
-        data-blobity-magnetic="false"
-        onClick={handleScroll}
-        aria-label="Scroll to Home Section"
-      >
-        <h4 className="rounded py-2 px-2 text-[12px] sm:px-4 sm:text-[14px] md:py-1 md:px-4">
-          Home
+        <h4 className="py-2 px-4 text-[14px] font-semibold text-[#e4ded7] sm:block  md:text-[16px] lg:block">
+          View Resume
         </h4>
       </Link>
+
       <Link
         href="#work"
         data-blobity-magnetic="false"
@@ -63,17 +47,6 @@ const NavBar = () => {
       >
         <h4 className="rounded py-2 px-2 text-[12px] sm:px-4 sm:text-[14px] md:py-1 md:px-4">
           About
-        </h4>
-      </Link>
-
-      <Link
-        href="#contact"
-        data-blobity-magnetic="false"
-        onClick={handleScroll}
-        aria-label="Scroll to Contact Section"
-      >
-        <h4 className="rounded py-2 px-2 text-[12px] sm:px-4 sm:text-[14px] md:py-1 md:px-4">
-          Contact
         </h4>
       </Link>
     </nav>
