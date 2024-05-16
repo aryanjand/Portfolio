f4d1d4e0dab87aac1ca7a483e5b5a00f16a446c8
aryan <aryan.jand@gmail.com>
2024-05-16 11:36:55 -0700

    New Font added

---
+++ app/fonts/leagueSpartan.ts
@@ -0,0 +1,51 @@
+import localFont from "next/font/local";
+
+export const leagueSpartan = localFont({
+  src: [
+    {
+      path: "./LeagueSpartan-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./LeagueSpartan-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./LeagueSpartan-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./LeagueSpartan-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./LeagueSpartan-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./LeagueSpartan-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./LeagueSpartan-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./LeagueSpartan-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./LeagueSpartan-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+  ],
+});
